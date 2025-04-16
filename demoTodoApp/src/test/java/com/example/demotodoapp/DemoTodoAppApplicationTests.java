418a104c8d908c148bb13a3c165724555ca01313
xdung04 <157205064+xdung04@users.noreply.github.com>
2025-04-16 11:36:10 +0700

    Initial commit

---
+++ demoTodoApp/src/test/java/com/example/demotodoapp/DemoTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demotodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTodoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
